19eac3e7ee3170214b6cc0b038a38ae0f9bac0e1
Sander <fristislekker@proton.me>
2026-06-15 13:56:23 +0200

    feat: add PDO database connection

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once "core/Router.php";
 require_once "core/Helper.php";
 
 $config = require "config/Config.php";
+$db = new Database($config);
 $router = new Router($config);
 
 $router->get('/', function() {
