b465c00bf463df89da9e39e13be582d9ab11ebff
Sander <fristislekker@proton.me>
2026-06-15 10:52:56 +0200

    feat: create basic project structure

---
+++ config/Config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    "app_name" => "Trans Free",
+
+    "base_path" => "/transfree",
+
+    "database" => [
+        "host" => "",
+        "dbname" => "",
+        "username" => "",
+        "password" => "",
+    ],
+];
