1cfda66165f4c55877d4397a5cb2344f062a4ad2
Sander <fristislekker@proton.me>
2026-06-17 13:44:22 +0200

    feat: update navbar and hero section for user authentication

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -15,7 +15,17 @@ require_once __DIR__ . "/../../config/Config.php";
         <li><a href="<?php echo $config["base_path"] ?>/upload">Upload</a></li>
     </ul>
     <div class="navbar-auth">
-        <a href="<?php echo $config["base_path"] ?>/login" class="navbar-login">Login</a>
-        <a href="<?php echo $config["base_path"] ?>/register" class="navbar-register">Register</a>
+        <?php
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION["user_id"])): ?>
+            <span class="navbar-login">Hello, <?php echo htmlspecialchars($_SESSION["username"] ?? 'User'); ?></span>
+            <a href="<?php echo $config["base_path"] ?>/logout" class="navbar-register">Logout</a>
+        <?php else: ?>
+            <a href="<?php echo $config["base_path"] ?>/login" class="navbar-login">Login</a>
+            <a href="<?php echo $config["base_path"] ?>/register" class="navbar-register">Register</a>
+        <?php endif; ?>
     </div>
 </nav>
